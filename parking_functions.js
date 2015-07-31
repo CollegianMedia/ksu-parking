043747e599b9724b587b3648d3019ac569c81085
George Walker <george@georgewwalker.com>
2015-07-31 09:38:50 -0500

    Additional fixes to js file
    added ‘status-‘ that was missing for the classes.

--- parking_functions.js
+++ parking_functions.js
@@ -12,15 +12,15 @@
 // Function for setting status
 function set_status(status_location, the_status) {
 	remove_status(status_location);
-	jQuery('p.' + status_location).html(the_status);
-	jQuery('.' + status_location).addClass('status-' + the_status);
+	jQuery('p.status-' + status_location).html(the_status);
+	jQuery('.status-' + status_location).addClass('status-' + the_status);
 }
 	
 // Function to remove all statuses
 function remove_status(status_location) {
-	jQuery('.' + status_location).removeClass('status-unknown');
-	jQuery('.' + status_location).removeClass('status-open');
-	jQuery('.' + status_location).removeClass('status-full');
+	jQuery('.status-' + status_location).removeClass('status-unknown');
+	jQuery('.status-' + status_location).removeClass('status-open');
+	jQuery('.status-' + status_location).removeClass('status-full');
 }
 
 // Function for alerts
