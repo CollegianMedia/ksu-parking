e8d15a7e09eb44d9dcc97653f39672d382f02950
George Walker <george@georgewwalker.com>
2015-07-31 09:34:23 -0500

    Fixes to js file
    Replaced ‘-‘ in function parameters with ‘_’

--- parking_functions.js
+++ parking_functions.js
@@ -10,17 +10,17 @@
 
 
 // Function for setting status
-function set_status(status-location, the-status) {
-	remove_status(status-location);
-	jQuery('p.' + status-location).html(the-status);
-	jQuery('.' + status-location).addClass('status-' + the-status);
+function set_status(status_location, the_status) {
+	remove_status(status_location);
+	jQuery('p.' + status_location).html(the_status);
+	jQuery('.' + status_location).addClass('status-' + the_status);
 }
 	
 // Function to remove all statuses
-function remove_status(status-location) {
-	jQuery('.' + status-location).removeClass('status-unknown');
-	jQuery('.' + status-location).removeClass('status-open');
-	jQuery('.' + status-location).removeClass('status-full');
+function remove_status(status_location) {
+	jQuery('.' + status_location).removeClass('status-unknown');
+	jQuery('.' + status_location).removeClass('status-open');
+	jQuery('.' + status_location).removeClass('status-full');
 }
 
 // Function for alerts
